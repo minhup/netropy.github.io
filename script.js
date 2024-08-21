1d939a29ded35fdcad756666e776236280e5200b
Minh Truong <minh@Minhs-Laptop.local>
2024-08-22 00:06:43 +0900

    replace onclick event and document query

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function toggleMenu() {
-  var navbarLinks = document.getElementById("navbar-links");
-  var toggleButton = document.getElementById("toggle-button");
+document.addEventListener('DOMContentLoaded', function() {
+  var toggleButton = document.querySelector('#toggle-button');
+  var navbarLinks = document.querySelector('#navbar-links');
 
-  if (navbarLinks.classList.contains("active")) {
-      navbarLinks.classList.remove("active");
-      toggleButton.innerHTML = "&#9776;"; // Change to hamburger menu
-  } else {
-      navbarLinks.classList.add("active");
-      toggleButton.innerHTML = "&times;"; // Change to 'X'
-  }
-}
+  toggleButton.addEventListener('click', function() {
+      if (navbarLinks.classList.contains('active')) {
+          navbarLinks.classList.remove('active');
+          toggleButton.innerHTML = '&#9776;'; // Change to hamburger menu
+      } else {
+          navbarLinks.classList.add('active');
+          toggleButton.innerHTML = '&times;'; // Change to 'X'
+      }
+  });
+});
