995f6fff9b120d2fdb84839ea5f8a0844d6228a4
Minh Truong <minh@Minhs-Laptop.local>
2024-08-21 23:52:43 +0900

    fix navigation toggle

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.nav-menu');
+function toggleMenu() {
+  var navbarLinks = document.getElementById("navbar-links");
+  var toggleButton = document.getElementById("toggle-button");
 
-navToggle.addEventListener('click', () => {
-  navToggle.classList.toggle('open');
-  navMenu.classList.toggle('show');
-});
+  if (navbarLinks.classList.contains("active")) {
+      navbarLinks.classList.remove("active");
+      toggleButton.innerHTML = "&#9776;"; // Change to hamburger menu
+  } else {
+      navbarLinks.classList.add("active");
+      toggleButton.innerHTML = "&times;"; // Change to 'X'
+  }
+}
